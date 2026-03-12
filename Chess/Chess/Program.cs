bd8f29dca5294dbfa7a32057d9d29373b852281a
Wiktoria <wiktoriaajaworowska@gmail.com>
2026-03-12 11:05:45 +0100

    Added Checkmate, Check and FindKing, added and fixed loop for game

--- Chess/Chess/Program.cs
+++ Chess/Chess/Program.cs
@@ -111,55 +111,145 @@ namespace Chess
             currPiece.Row = rowNew;
             currPiece.Col = colNew;
             currPiece.HasMoved = true;
-            
         }
+        public static Piece FindKing(bool isWhiteTurn, Piece[,] board)
+        {
+            Piece king = null;
+            for (int r = 0; r < 8; r++)
+            {   
+                for (int c = 0; c < 8; c++)
+                {
+                    Piece p = board[r, c];
+                    if (p != null && p is King && p.IsWhite == isWhiteTurn)
+                    {
+                        king = p;
+                        return king;
+                    }
+                }
+            }
+            return king;
+        }
+        public static bool IsCheck(bool isWhiteTurn, Piece[,] board)
+        {
+            Piece king = FindKing(isWhiteTurn, board);
+            if (king == null) return false;
+
+            int kingRow = king.Row;
+            int kingCol = king.Col;
+            foreach(var p in board)
+            {
+                if (p != null && p.IsWhite != isWhiteTurn)
+                {
+                    List<(int, int)> possMoves = p.GetMoves(board);
+                    if(possMoves.Contains((kingRow, kingCol))){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public static bool IsCheckmate(bool isWhiteTurn, Piece[,] board)
+        {
+            if(!IsCheck(isWhiteTurn, board)) return false;
+
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 8; c++)
+                {
+                    Piece p = board[r,c];
+                    if (p != null && p.IsWhite == isWhiteTurn)
+                    {
+                        List<(int, int)> possMoves = p.GetMoves(board);
+                        foreach (var move in possMoves)
+                        {
+                            Piece[,] copyBoard = (Piece[,])board.Clone();
+
+                            copyBoard[move.Item1, move.Item2] = copyBoard[r,c];
+                            copyBoard[r, c] = null;
+
+                            if (!IsCheck(isWhiteTurn, copyBoard)) return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        
+        //public static bool IsStalemate(bool isWhiteTurn, Piece[,] board)
+        //{
+
+        //}
         public static void Main(string[] args)
         {
             Piece[,] Board = new Piece[8, 8];
             bool IsWhiteTurn = true;
+            bool gameOver = false;
             InitializeBoard(Board);
-            PrintBoard(Board);
             
-            Console.WriteLine("Turn: " + (IsWhiteTurn ? "White" : "Black") +"\nChoose your piece (ex. F5):");
-            string pos = Console.ReadLine();
-            while (!ValidationSelectPiece(pos, Board, IsWhiteTurn))
+            while (!gameOver)
             {
-                Console.WriteLine("Invalid choice, try again.");
-                pos = Console.ReadLine();
-            }
-
-            List<(int,int)> moves = SelectPiece(pos, Board);
-            Console.WriteLine("Possible moves: ");
-            foreach(var move in moves)
-            {
-                Console.Write(ToChessNotation(move.Item1, move.Item2)+" ");
-            }
-
-            Console.WriteLine("Select your move from possible moves: ");
-            string chosenMove = Console.ReadLine();
-            while (chosenMove.Length != 2) 
-            {
-                Console.WriteLine("Invalid choice, try again. You need to type exacly in a way its displayed (ex. A2)");
-            }
-
-            int choseRow = 8 - (chosenMove[1] - '0');
-            int choseCol = char.ToUpper(chosenMove[0]) - 'A';
-            while (!moves.Contains((choseRow,choseCol)))
-            {
-                Console.WriteLine("Invalid choice, try again. You need to type exacly in a way its displayed (ex. A2)");
-                chosenMove = Console.ReadLine();
-
-                if(chosenMove.Length != 2)
+                PrintBoard(Board);
+                Console.WriteLine("Turn: " + (IsWhiteTurn ? "White" : "Black") + "\nChoose your piece (ex. F5):");
+                string pos = Console.ReadLine();
+                while (!ValidationSelectPiece(pos, Board, IsWhiteTurn))
                 {
+                    Console.WriteLine("Invalid choice, try again.");
+                    pos = Console.ReadLine();
+                }
+
+                List<(int, int)> moves = SelectPiece(pos, Board);
+                Console.WriteLine("Possible moves: ");
+                foreach (var move in moves)
+                {
+                    Console.Write(ToChessNotation(move.Item1, move.Item2) + " ");
+                }
+
+                Console.WriteLine("Select your move from possible moves: ");
+                string chosenMove = Console.ReadLine();
+                while (chosenMove.Length != 2)
+                {
+                    Console.WriteLine("Invalid choice, try again. You need to type exacly in a way its displayed (ex. A2)");
+                }
+
+                int choseRow = 8 - (chosenMove[1] - '0');
+                int choseCol = char.ToUpper(chosenMove[0]) - 'A';
+                while (!moves.Contains((choseRow, choseCol)))
+                {
+                    Console.WriteLine("Invalid choice, try again. You need to type exacly in a way its displayed (ex. A2)");
+                    chosenMove = Console.ReadLine();
+
+                    if (chosenMove.Length != 2)
+                    {
+                        continue;
+                    }
+
+                    choseRow = 8 - (chosenMove[1] - '0');
+                    choseCol = chosenMove[0] - 'A';
+                }
+                MovePiece(pos, choseRow, choseCol, Board);
+
+                if (IsCheckmate(IsWhiteTurn, Board))
+                {
+                    PrintBoard(Board);
+                    Console.WriteLine("Checkmate!"+(IsWhiteTurn ? "White" : "Black")+ " wins!");
+                    gameOver = true;
+                    continue;
+                }
+                else if (IsCheck(IsWhiteTurn, Board))
+                {
+                    Console.WriteLine("Check!");
+                }
+                else if (IsStalemate(IsWhiteTurn, Board))
+                {
+                    PrintBoard(Board);
+                    Console.WriteLine("Stalemate! It's a draw!");
+                    gameOver = true;
                     continue;
                 }
 
-                choseRow = 8 - (chosenMove[1] - '0');
-                choseCol = chosenMove[0] - 'A';
+                IsWhiteTurn = !IsWhiteTurn;
             }
-            MovePiece(pos, choseRow, choseCol, Board);
-            IsWhiteTurn = !IsWhiteTurn;
-            PrintBoard(Board);
+            
 
         }
     }
